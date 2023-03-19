d8c75b4f34d4c8c7cf82671e045fee01aa9f7f4e
Rodgers Ogada <ogadarodgers@gmail.com>
2023-03-19 20:12:56 +0300

    Price validation for restaurant pizza

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -1,4 +1,10 @@
 class RestaurantPizza < ApplicationRecord
+    validates :price, 
+        inclusion: {
+        in: (1..30),
+        message: 'Price must be between 1 and 30'
+        }
+
     belongs_to :restaurant
     belongs_to :pizza
 end
