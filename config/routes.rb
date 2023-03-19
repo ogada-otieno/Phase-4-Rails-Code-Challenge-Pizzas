1242f0e8b55c6988331f393309285dceb61a0cc2
Rodgers Ogada <ogadarodgers@gmail.com>
2023-03-19 19:54:06 +0300

    created models and migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :restaurants
+  resources :pizzas
+  resources :restaurant_pizzas
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
