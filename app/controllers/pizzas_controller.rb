31ffbb870e0e5db30f434b30fee53a6e13f094e5
Rodgers Ogada <ogadarodgers@gmail.com>
2023-03-20 15:50:53 +0300

    For deployment.

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -2,7 +2,6 @@ class PizzasController < ApplicationController
     # GET /pizzas
     def index
         pizzas = Pizza.all
-        # render json: pizzas, status: :ok
         render json: pizzas, status: :ok
     end
 
